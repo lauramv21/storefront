b1ae5c12b5a47d08ba5cfaa5398ad0559d66e88d
Laura Mejia <laura.mejia@talosdigital.com>
2019-07-23 15:16:45 -0500

    show images

--- src/app/components/product-summary/product-summary.component.ts
+++ src/app/components/product-summary/product-summary.component.ts
@@ -12,6 +12,8 @@ export class ProductSummaryComponent implements OnInit {
   productSummary: Observable<Product>;
   productInformation: any;
   hasImagesCarousel: boolean;
+  imagesGallery: any;
+  activeImage: string;
 
   constructor(productService: CurrentProductService) {
     this.productSummary = productService.getProduct();
@@ -21,7 +23,23 @@ export class ProductSummaryComponent implements OnInit {
     this.productSummary.subscribe((value) => {
       this.productInformation = value;
       this.hasImagesCarousel = this.productInformation ? this.productInformation.images.GALLERY.length > 1 : false;
+      this.setImages();
       console.log(this.productInformation);
     });
+  }
+
+  private setImages() {
+    this.imagesGallery = [];
+    if (this.productInformation) {
+      this.productInformation.images.GALLERY.forEach((image) => {
+        this.imagesGallery.push({thumbnail: image.thumbnail.url, zoom: image.zoom.url});
+        this.imagesGallery.push({thumbnail: image.thumbnail.url, zoom: image.zoom.url});
+      });
+      this.activeImage = this.imagesGallery[0].zoom;
+    }
+  }
+
+  changeImageToDisplay(image: string) {
+    this.activeImage = image;
   }
 }
